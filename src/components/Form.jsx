f52835e0bcc7f869f358e1279a2f380d60c5df44
Star <mmdakane@rocketmail.com>
2026-04-16 22:20:39 +0200

    form errors to be continued

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,43 +1,70 @@
+import { useContext } from "react"
 import Calculator from "../../assets/images/icon-calculator.svg"
+import { GlobalContext } from "../contexts/GlobalContext"
 
 const Form = () => {
+
+    const { formValues, setFormValues, handleFormInput, initialValue, handleSubmit, formErrors } = useContext(GlobalContext);
+
+    console.log(formErrors);
+
     return (
-        <form id="form" className='w-full lg:w-126 py-8 px-6 md:p-10 flex flex-col gap-6 md:gap-10'>
+        <form id="form" onSubmit={handleSubmit} className='w-full lg:w-126 py-8 px-6 md:p-10 flex flex-col gap-6 md:gap-10'>
             <div className='flex flex-col md:flex-row gap-2 md:justify-between'>
                 <h1 className='text-[24px] leading-[125%] tracking-[0%] text-slate-900 font-bold'>Mortgage Calculator</h1>
-                <button type="button" className='text-[16px] text-[#4E6E7E] text-left leading-[150%] tracking-[0%] font-medium underline cursor-pointer'>Clear All</button>
+                <button type="button" onClick={() => setFormValues(initialValue)} className='text-[16px] text-[#4E6E7E] text-left leading-[150%] tracking-[0%] font-medium underline cursor-pointer'>Clear All</button>
             </div>
             <div className='grid md:grid-cols-2 gap-6'>
                 <div className='col-span-2 flex flex-col gap-3'>
                     <label htmlFor="amount" className='text-[16px] text-[#4E6E7E] text-left leading-[150%] tracking-[0%] font-medium'>Mortgage Amount</label>
-                    <div className='flex border border-[#6B94A8] rounded-sm'>
-                        <p className='w-11 px-4 py-3 text-[18px] leading-[125%] tracking-[0%] font-bold text-[#4E6E7E] bg-[#E4F4FD]'>{"\u00A3"}</p>
-                        <input type="text" name="" id="amount" className='w-full px-4 text-[18px] leading-[125%] tracking-[0%] text-slate-900 font-bold placeholder-slate-900 cursor-pointer' placeholder='300000' />
+                    <div className={`flex border ${formErrors.amount ? 'border-[#D73328]' : 'border-[#6B94A8]'}  rounded-sm`}>
+                        <p className={`w-11 px-4 py-3 text-[18px] leading-[125%] tracking-[0%] font-bold ${formErrors.amount ? 'text-white' : 'text-[#4E6E7E]'} ${formErrors.amount ? 'bg-[#D73328]' : 'bg-[#E4F4FD]'} `}>{"\u00A3"}</p>
+                        <input type="number" name="amount" id="amount" value={formValues.amount} onChange={handleFormInput} className='w-full px-4 text-[18px] leading-[125%] tracking-[0%] text-slate-900 font-bold placeholder-slate-900 appearance-none [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none cursor-pointer' />
                     </div>
+                    {formErrors.amount && (
+                        <p className="text-[#D73328] text-[14px] leading-[150%] font-medium">
+
+                            {formErrors.amount}
+                        </p>
+                    )}
                 </div>
                 <div className='col-span-2 md:col-span-1 flex flex-col gap-3'>
-                    <label htmlFor="term" className='text-[16px] text-[#4E6E7E] text-left leading-[150%] tracking-[0%] font-medium'>Mortgage Amount</label>
+                    <label htmlFor="term" className='text-[16px] text-[#4E6E7E] text-left leading-[150%] tracking-[0%] font-medium'>Mortgage Term</label>
                     <div className='flex border border-[#6B94A8] rounded-sm'>
-                        <input type="text" name="" id="term" className='w-full px-4 text-[18px] leading-[125%] tracking-[0%] text-slate-900 font-bold placeholder-slate-900 cursor-pointer' placeholder='25' />
+                        <input type="number" max={30} name="term" id="term" value={formValues.term} onChange={handleFormInput} className='w-full px-4 text-[18px] leading-[125%] tracking-[0%] text-slate-900 font-bold placeholder-slate-900 appearance-none [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none cursor-pointer' />
                         <p className='w-20 px-4 py-3 text-[18px] leading-[125%] tracking-[0%] font-bold text-[#4E6E7E] bg-[#E4F4FD]'>years</p>
                     </div>
+                    {formErrors.term && (
+                        <p className="text-[#D73328] text-[14px] leading-[150%] font-medium">
+
+                            {formErrors.term}
+                        </p>
+                    )}
                 </div>
                 <div className='col-span-2 md:col-span-1 flex flex-col gap-3'>
-                    <label htmlFor="rate" className='text-[16px] text-[#4E6E7E] text-left leading-[150%] tracking-[0%] font-medium'>Mortgage Amount</label>
+                    <label htmlFor="rate" className='text-[16px] text-[#4E6E7E] text-left leading-[150%] tracking-[0%] font-medium'>Interest Rate</label>
                     <div className='flex border border-[#6B94A8] rounded-sm'>
-                        <input type="text" name="" id="range" className='w-full px-4 text-[18px] leading-[125%] tracking-[0%] text-slate-900 font-bold placeholder-slate-900 cursor-pointer' placeholder='5.25' />
+                        <input type="number" max={20} name="rate" value={formValues.rate} id="rate" onChange={handleFormInput} className='w-full px-4 text-[18px] leading-[125%] tracking-[0%] text-slate-900 font-bold placeholder-slate-900 appearance-none [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none cursor-pointer' />
                         <p className='w-12.5 px-4 py-3 text-[18px] leading-[125%] tracking-[0%] font-bold text-[#4E6E7E] bg-[#E4F4FD]'>%</p>
                     </div>
+                    {formErrors.rate && (
+                        <p className="text-[#D73328] text-[14px] leading-[150%] font-medium">
+
+                            {formErrors.rate}
+                        </p>
+                    )}
                 </div>
                 <fieldset className='col-span-2 flex flex-col gap-3'>
                     <legend className='text-[16px] text-[#4E6E7E] text-left leading-[150%] tracking-[0%] font-medium'>Mortgage Type</legend>
-                    <label className='border border-[#6B94A8] rounded-sm px-4 gap-4 h-12 flex items-center cursor-pointer has-checked:bg-[#D8DB2F]/10 group'>
+                    <label className='border border-[#6B94A8] rounded-sm px-4 gap-4 h-12 flex items-center cursor-pointer has-checked:bg-[#D8DB2F]/10 has-checked:border-[#D8DB2F] group'>
                         <div className="grid place-items-center w-6 h-6">
                             <input
                                 type="radio"
                                 name="type"
+                                value="repayment"
+                                checked={formValues.type === 'repayment'}
+                                onChange={handleFormInput}
                                 className="peer col-start-1 row-start-1 appearance-none w-6 h-6 border-2 border-gray-400 rounded-full checked:border-[#D8DB2F] focus:ring-0 cursor-pointer"
-                                defaultChecked
                             />
                             <div className="col-start-1 row-start-1 w-3 h-3 rounded-full bg-[#D8DB2F] opacity-0 peer-checked:opacity-100 pointer-events-none transition-opacity" />
                         </div>
@@ -45,11 +72,14 @@ const Form = () => {
                             Repayment
                         </span>
                     </label>
-                    <label className='border border-[#6B94A8] rounded-sm px-4 gap-4 h-12 flex items-center cursor-pointer has-checked:bg-[#D8DB2F]/10 group'>
+                    <label className='border border-[#6B94A8] rounded-sm px-4 gap-4 h-12 flex items-center cursor-pointer has-checked:bg-[#D8DB2F]/10 has-checked:border-[#D8DB2F] group'>
                         <div className="grid place-items-center w-6 h-6">
                             <input
                                 type="radio"
                                 name="type"
+                                value="interestOnly"
+                                checked={formValues.type === 'interestOnly'}
+                                onChange={handleFormInput}
                                 className="peer col-start-1 row-start-1 appearance-none w-6 h-6 border-2 border-gray-400 rounded-full checked:border-[#D8DB2F] focus:ring-0 cursor-pointer"
                             />
                             <div className="col-start-1 row-start-1 w-3 h-3 rounded-full bg-[#D8DB2F] opacity-0 peer-checked:opacity-100 pointer-events-none transition-opacity" />
@@ -58,6 +88,12 @@ const Form = () => {
                             Interest Only
                         </span>
                     </label>
+                    {formErrors.type && (
+                        <p className="text-[#D73328] text-[14px] leading-[150%] font-medium">
+
+                            {formErrors.type}
+                        </p>
+                    )}
                 </fieldset>
             </div>
             <div className="md:text-left">
--- src/contexts/GlobalContext.jsx
+++ src/contexts/GlobalContext.jsx
@@ -4,11 +4,69 @@ import { createContext, useState } from "react";
 const GlobalContext = createContext()
 
 const GlobalProvider = ({ children }) => {
+
+    const initialValue = { amount: "", term: "", rate: "", type: "" };
+
     const [showResults, setShowResults] = useState(false);
+    const [formValues, setFormValues] = useState(initialValue);
+    const [formErrors, setFormErrors] = useState({});
+
+    const handleFormInput = (e) => {
+        const { name, value, type, checked } = e.target;
+        setFormValues(prev => ({
+            ...prev,
+            [name]: type === 'checkbox' ? checked : value
+        }))
+    }
+
+    const validate = (form) => {
+        const errors = {};
+        if (!form.amount) {
+            errors.amount = "This field is required";
+        }
+        if (!form.term) {
+            errors.term = "This field is required";
+        }
+        if (!form.rate) {
+            errors.rate = "This field is required";
+        }
+        if (!form.type) errors.type = "This field is required";
+        return errors;
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const errors = validate(formValues)
+        setFormErrors(errors)
+
+        const noErrors = Object.keys(errors).length === 0;
+        if (noErrors) {
+            calculateMortgage(formValues);
+            setShowResults(true);
+        } else {
+            setShowResults(false);
+        }
+    }
+
+    const calculateMortgage = (values) => {
+        const amount = Number(values.amount);
+        const term = Number(values.term);
+        const rate = Number(values.rate) / 100 / 12;
+        const months = Number(values.term) * 12;
+
+        console.log({ amount, term, rate, months });
+    }
 
     const value = {
         showResults,
-        setShowResults
+        setShowResults,
+        formValues,
+        formErrors,
+        handleSubmit,
+        handleFormInput,
+        initialValue,
+        setFormValues
+
     }
 
     return (
