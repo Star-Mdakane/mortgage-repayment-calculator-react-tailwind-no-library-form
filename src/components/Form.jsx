04a72e47117a87171202c707eb7fa7955e41c836
Star <mmdakane@rocketmail.com>
2026-04-18 15:47:57 +0200

    interest

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -48,7 +48,26 @@ const Form = () => {
                 <div className='col-span-2 md:col-span-1 flex flex-col gap-3'>
                     <label htmlFor="rate" className='text-[16px] text-[#4E6E7E] text-left leading-[150%] tracking-[0%] font-medium'>Interest Rate</label>
                     <div className={`flex border ${formErrors.amount ? 'border-[#D73328]' : 'border-[#6B94A8]'}  rounded-sm`}>
-                        <input type="text" inputMode="decimal" pattern="[0-9]*" max={20} name="rate" value={formValues.rate} id="rate" onChange={handleFormInput} className='w-full px-4 text-[18px] leading-[125%] tracking-[0%] text-slate-900 font-bold placeholder-slate-900 appearance-none [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none cursor-pointer' />
+                        <input type="text"
+                            inputMode="decimal"
+                            pattern="[0-9]*"
+                            max={20}
+                            name="rate"
+                            value={formValues.rate}
+                            id="rate"
+                            onChange={(e) => {
+                                let val = e.target.value.replace(',', '.').replace(/[^0-9.]/g, '');
+
+                                const parts = val.split('.');
+                                if (parts.length > 2) {
+                                    val = parts[0] + '.' + parts.slice(1).join('');
+                                }
+
+                                setFormValues(prev => ({ ...prev, rate: val }));
+                            }
+                            }
+
+                            className='w-full px-4 text-[18px] leading-[125%] tracking-[0%] text-slate-900 font-bold placeholder-slate-900 appearance-none [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none cursor-pointer' />
                         <p className={`w-12.5 px-4 py-3 text-[18px] leading-[125%] tracking-[0%] font-bold ${formErrors.amount ? 'text-white' : 'text-[#4E6E7E]'} ${formErrors.amount ? 'bg-[#D73328]' : 'bg-[#E4F4FD]'}`}>%</p>
                     </div>
                     {formErrors.rate && (
