b295606fa420e1e301a6cb0b9ab2be6963d729c7
Star-Mdakane <101474151+Star-Mdakane@users.noreply.github.com>
2026-04-20 10:22:40 +0200

    Update Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -23,7 +23,7 @@ const Form = () => {
                     <label htmlFor="amount" className='text-[16px] text-[#4E6E7E] text-left leading-[150%] tracking-[0%] font-medium'>Mortgage Amount</label>
                     <div className={`flex border ${formErrors.amount ? 'border-[#D73328]' : 'border-[#6B94A8]'}  rounded-sm`}>
                         <p className={`w-11 px-4 py-3 text-[18px] leading-[125%] tracking-[0%] font-bold ${formErrors.amount ? 'text-white' : 'text-[#4E6E7E]'} ${formErrors.amount ? 'bg-[#D73328]' : 'bg-[#E4F4FD]'} `}>{"\u00A3"}</p>
-                        <input type="number" inputMode="decimal" pattern="[0-9]*[.]?[0-9]*" name="amount" id="amount" value={formValues.amount} onChange={handleFormInput} className='w-full px-4 text-[18px] leading-[125%] tracking-[0%] text-slate-900 font-bold placeholder-slate-900 appearance-none [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none cursor-pointer' />
+                        <input type="number" inputMode="decimal" name="amount" id="amount" value={formValues.amount} onChange={handleFormInput} className='w-full px-4 text-[18px] leading-[125%] tracking-[0%] text-slate-900 font-bold placeholder-slate-900 appearance-none [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none cursor-pointer' />
                     </div>
                     {formErrors.amount && (
                         <p role="alert" className="text-[#D73328] text-[14px] leading-[150%] font-medium">
@@ -35,7 +35,7 @@ const Form = () => {
                 <div className='col-span-2 md:col-span-1 flex flex-col gap-3'>
                     <label htmlFor="term" className='text-[16px] text-[#4E6E7E] text-left leading-[150%] tracking-[0%] font-medium'>Mortgage Term</label>
                     <div className={`flex border ${formErrors.amount ? 'border-[#D73328]' : 'border-[#6B94A8]'}  rounded-sm`}>
-                        <input type="number" inputMode="decimal" pattern="[0-9]*[.]?[0-9]*" max={30} name="term" id="term" value={formValues.term} onChange={handleFormInput} className='w-full px-4 text-[18px] leading-[125%] tracking-[0%] text-slate-900 font-bold placeholder-slate-900 appearance-none [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none cursor-pointer' />
+                        <input type="number" inputMode="decimal" max={30} name="term" id="term" value={formValues.term} onChange={handleFormInput} className='w-full px-4 text-[18px] leading-[125%] tracking-[0%] text-slate-900 font-bold placeholder-slate-900 appearance-none [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none cursor-pointer' />
                         <p className={`w-20 px-4 py-3 text-[18px] leading-[125%] tracking-[0%] font-bold ${formErrors.amount ? 'text-white' : 'text-[#4E6E7E]'} ${formErrors.amount ? 'bg-[#D73328]' : 'bg-[#E4F4FD]'}`}>years</p>
                     </div>
                     {formErrors.term && (
@@ -50,7 +50,6 @@ const Form = () => {
                     <div className={`flex border ${formErrors.amount ? 'border-[#D73328]' : 'border-[#6B94A8]'}  rounded-sm`}>
                         <input type="number"
                             inputMode="decimal"
-                            pattern="[0-9]*[.]?[0-9]*"
                             max={20}
                             name="rate"
                             value={formValues.rate}
@@ -88,7 +87,7 @@ const Form = () => {
                                 onChange={handleFormInput}
                                 className="peer col-start-1 row-start-1 appearance-none w-6 h-6 border-2 border-gray-400 rounded-full checked:border-[#D8DB2F] focus:ring-0 cursor-pointer"
                             />
-                            <div className="col-start-1 row-start-1 w-3 h-3 rounded-full bg-[#D8DB2F] opacity-0 peer-checked:opacity-100 pointer-events-none transition-opacity" />
+                            <span className="col-start-1 row-start-1 w-3 h-3 rounded-full bg-[#D8DB2F] opacity-0 peer-checked:opacity-100 pointer-events-none transition-opacity" />
                         </span>
                         <span className='text-[18px] leading-[125%] font-bold text-slate-900 flex-1'>
                             Repayment
@@ -105,7 +104,7 @@ const Form = () => {
                                 onChange={handleFormInput}
                                 className="peer col-start-1 row-start-1 appearance-none w-6 h-6 border-2 border-gray-400 rounded-full checked:border-[#D8DB2F] focus:ring-0 cursor-pointer"
                             />
-                            <div className="col-start-1 row-start-1 w-3 h-3 rounded-full bg-[#D8DB2F] opacity-0 peer-checked:opacity-100 pointer-events-none transition-opacity" />
+                            <span className="col-start-1 row-start-1 w-3 h-3 rounded-full bg-[#D8DB2F] opacity-0 peer-checked:opacity-100 pointer-events-none transition-opacity" />
                         </span>
                         <span className='text-[18px] leading-[125%] font-bold text-slate-900 flex-1'>
                             Interest Only
